94a1d74d0a2e53cce3ebc4b3fb01b90a401f4079
VIctor <colen.victor@icloud.com>
2021-10-31 23:13:37 -0300

    prepared the list to networking

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List(posts) { posts in
+                Text(posts.title)
+            }
+            .navigationTitle("H4X0R NEWS")
+        }
     }
 }
 
@@ -19,3 +23,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+ Post(id: "1", title: "hey there"),
+ Post(id: "2", title: "alo"),
+ Post(id: "3", title: "familia"),
+ Post(id: "4", title: "alooo")
+]
